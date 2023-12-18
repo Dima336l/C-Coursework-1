2c6e039212ecf1aa370eab1ecce47293d9b410ca
Dumitru Nirca <Nircadmitrii@icloud.com>
2023-12-18 21:26:11 +0000

    Created Librarian Subclass, Tested Default and Parameterized Constructors

---
+++ Librarian.cpp
@@ -0,0 +1,33 @@
+#include "Librarian.h"
+#include <string>
+
+Librarian::Librarian() {
+  salary = 0;
+  staffId = 0;
+}
+Librarian::Librarian(int staffID, std::string name, std::string address, std::string email, int sal) {
+  //Seting values using setters from base class
+  setName(name);
+  setAddress(address);
+  setEmail(email);
+  
+  //Setting Libriarian-specific members
+  Librarian::setStaffID(staffID);
+  Librarian::setSalary(sal);
+}
+void Librarian::setStaffID(int id) {
+  staffId = id;
+}
+
+void Librarian::setSalary(int sal) {
+  salary = sal;
+}
+
+int Librarian::getStaffID() {
+  return staffId;
+}
+
+int Librarian::getSalary() {
+  return salary;
+}
+
